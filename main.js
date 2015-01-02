f48589b181d312aa827edcc236afe5ee4551686e
Phil Ellsworth <philip.d.ellsworth@gmail.com>
2015-01-02 14:56:32 -0700

    Click Me to Click Here

---
+++ main.js
@@ -0,0 +1,18 @@
+Router.configure({
+   layoutTemplate: 'layout',
+   yieldTemplate: {
+      header: {
+         to: 'header'
+      },
+      footer: {
+         to: 'footer'
+      }
+   }
+});
+        
+
+Router.map(function() {
+  this.route('home', {path: '/'});
+  this.route('about');
+  this.route('contact');
+});
